00ba1236a0dce7d64fbb4f2dc3e028f619bfc84d
Cleber Albuquerque <cleber.lop@gmail.com>
2016-01-11 01:31:32 -0700

    typedjs done

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -6,4 +6,20 @@ angular.module('mySite').controller('homeCtrl', function($scope, $timeout){
           window.onload = $scope.photoMain = true;
         }, 5000);
 
+
+    // typed.js function
+
+
+$(function(){
+    $(".element").typed({
+      strings: [" WELCOME TO MY WEBSITE!", "I AM A WEB DEVELOPER...", "...WORKING TO MAKE THE INTERNET LOOK AWESOME"],
+      typeSpeed: 40,
+      loop: true,
+      backDelay: 1500,
+      loopCount: false,
+      showCursor: true,
+
+    });
+  });
+
 });
